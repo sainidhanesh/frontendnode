1c6a371deba7b12c305737fef00c5ffb42479037
d05i <137749424+d05i@users.noreply.github.com>
2023-10-28 15:52:00 +0530

    layout fix

--- assets/js/main.js
+++
@@ -1,4 +0,0 @@
-const toggle = document.getElementById("toggle")
-const nav = document.querySelector("header nav")
-
-toggle.onclick = () => nav.classList.contains("hide") ? nav.classList.replace("hide", "grid") : nav.classList.replace("grid", "hide")
